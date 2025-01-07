9e8253e925f189d69f51a20bc8fbc23566178252
ShababAhmed <shabab.ahmed2000sa@gmail.com>
2025-01-07 14:51:40 +0600

    creating store and redux dev tools extension

---
+++ frontend/src/store.js
@@ -0,0 +1,23 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk"; // Change from default import to named import
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// Combine your reducers (leave it empty if you don't have reducers yet)
+const reducer = combineReducers({});
+
+const initialState = {};
+
+// Define middleware
+const middleware = [thunk];
+
+// Check if window.__REDUX_DEVTOOLS_EXTENSION__ is available and apply it if necessary
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Create the store, using composeEnhancers with applyMiddleware
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware)) // Correct usage of composeEnhancers
+);
+
+export default store;
