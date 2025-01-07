30eb6e1e905e885d7f1d9066017ff8baeef722ad
ShababAhmed <shabab.ahmed2000sa@gmail.com>
2025-01-07 17:27:44 +0600

    Redux: Login+Logout reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { thunk } from "redux-thunk"; // Change from default import to named import
 import { composeWithDevTools } from "redux-devtools-extension";
+import { userLoginReducer } from "./reducers/userReducers";
 
 // Combine your reducers (leave it empty if you don't have reducers yet)
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  userLogin: userLoginReducer,
+});
 
 const initialState = {};
 
