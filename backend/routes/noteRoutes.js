28ed79822cd189aa673eec7204ca6788dde52bfd
ShababAhmed <shabab.ahmed2000sa@gmail.com>
2025-01-08 21:24:15 +0600

    Notes API: Protect Middleware

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { getNotes } = require("../controllers/noteController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").get(getNotes);
+router.route("/").get(protect, getNotes);
 // router.route("/create").post();
 // router.route("/:id").get().put().delete();
 
