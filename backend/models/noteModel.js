c351a77a4fe407d6c7dfb9f91c343040de33f416
ShababAhmed <shabab.ahmed2000sa@gmail.com>
2025-01-08 20:53:15 +0600

    Notes API: Notes Model

---
+++ backend/models/noteModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const noteSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const note = mongoose.model("Note", noteSchema);
+module.exports = note;
